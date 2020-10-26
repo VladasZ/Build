0a832d2cfcf1efd6848a6986db040bbd27ec1da0
Vladas Zakrevskis <146100@gmail.com>
2020-10-26 13:13:53 +0300

    path parent and info

--- Program.fs
+++ Program.fs
@@ -1,10 +1,15 @@
 ﻿module main
 open Path
 
-let path = new Path("pjt1")
+let path = new Path(".")
 
 [<EntryPoint>]
 let main args =
-    printfn "%A" path.spath
+    printfn "%A" path.info
+    printfn "%A" path.parent.info
+    printfn "%A" path.parent.parent.parent.parent.info
+    printfn "%A" path.parent.parent.parent.parent.parent.parent.info
+    printfn "%A" path.parent.parent.parent.parent.parent.parent.parent.info
+    printfn "%A" path.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.info
     0
     
--- System/Path.fs
+++ System/Path.fs
@@ -2,5 +2,11 @@
 
 type Path(path: string) =
     member this.path = path
-    member this.spath = "s" + path
+    member this.full = System.IO.Path.GetFullPath(path)
+    member this.is_root = obj.ReferenceEquals(System.IO.DirectoryInfo(this.full).Parent, null)
     
+    member this.parent = match this.is_root with
+    | true  -> new Path(this.full)
+    | false -> new Path(System.IO.Directory.GetParent(path).ToString())
+    
+    member this.info = "Path: " + this.full + " is root: " + this.is_root.ToString()
